14a3e211c86609a0808a9881324d8fb13e8b5c64
Ferrell4 <32971621+Ferrell4@users.noreply.github.com>
2022-08-07 04:52:59 -0400

    made the page more responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ window.onload = function () {
     window.addEventListener('scroll', function (e){
         // use this to console log pxs as you scroll down
         // console.log(window.pageYOffset);
-        if(window.pageYOffset > 100){
+        if(window.pageYOffset > 150){
             document.querySelector("header").classList.add('is-scrolling')
         } else{
             document.querySelector("header").classList.remove('is-scrolling')
