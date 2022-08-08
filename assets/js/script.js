425d5455ca0fd0f55119bf9219d7f25d969fc5ac
Ferrell4 <32971621+Ferrell4@users.noreply.github.com>
2022-08-08 12:25:54 -0400

    Finishing Touches
    For now...

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,21 @@ window.onload = function () {
         }
     });
 }
+
+const menu_button = document.querySelector(".menuButton button");
+const close_button = document.querySelector(".closeButton button");
+const closeOpen = document.querySelector(".closeButton");
+const menuClose = document.querySelector(".menuButton");
+const dropDown = document.querySelector(".dropDown");
+
+menu_button.onclick = ()=>{
+    dropDown.classList.add("activeDrop");
+    menuClose.classList.add("activeMenu");
+    closeOpen.classList.add("activeClose");
+} 
+
+close_button.onclick = ()=>{
+    closeOpen.classList.remove("activeClose");
+    menuClose.classList.remove("activeMenu");
+    dropDown.classList.remove("activeDrop");
+}
